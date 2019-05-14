3fb024bdf231439f4c2a0fb2813564cf22158963
Devalekhaa <50616161+Devalekhaa@users.noreply.github.com>
2019-05-14 15:56:57 +0530

    Add files via upload

---
+++ set11.java
@@ -0,0 +1,22 @@
+import java.util.Scanner
+class Pos
+{
+public static void main(String[] args)
+{
+int a;
+Scanner s=new Scanner(System.in);
+s=nextInt();
+if(a>0)
+{
+System.out.println("Positive");
+}
+else if(a<0)
+{
+System.out.println("Negative");
+}
+else
+{
+System.out.println("Zero");
+}
+}
+}
