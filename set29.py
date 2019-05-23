0629b9ad2fd00d7791f5acf49d7bb4c47f867a98
Devalekhaa <50616161+Devalekhaa@users.noreply.github.com>
2019-05-23 15:16:06 +0530

    Create set29.py

---
+++ set29.py
@@ -0,0 +1,13 @@
+def factorial(num):
+    if num == 1:
+        return num
+    else:
+        return num * factorial(num - 1)
+num = int(input())
+if num < 0:
+    print(num)
+elif num == 0:
+    print()
+else:
+    print(factorial(num))
+    
