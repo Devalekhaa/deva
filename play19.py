4b2106b8fa2b3d7938f684a6baf8e2e69a39794d
Devalekhaa <50616161+Devalekhaa@users.noreply.github.com>
2019-06-03 16:53:12 +0530

    Update play19.py

--- play19.py
+++ play19.py
@@ -1,18 +1,18 @@
-k=int(input())
-n=2
+d=int(input())
+e=2
 l=[]
-l1=[]
-for n in range(n,k+1):
+v=[]
+for e in range(e,d+1):
   i=2
-  while i<n:
-    if n%i==0:
+  while i<e:
+    if e%i==0:
       break
     i=i+1
-  if n==i:
-    l.append(n)
+  if e==i:
+    l.append(e)
 for i in l:
-  if k%i==0:
-    l1.append(i)
-for i in range(0,len(l1)-1):
-  print(l1[i],end=" ")
-print(l1[len(l1)-1])
+  if d%i==0:
+    v.append(i)
+for i in range(0,len(v)-1):
+  print(v[i],end=" ")
+print(v[len(v)-1])
