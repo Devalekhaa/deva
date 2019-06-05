fed47bf539c1af008fe506eacfc694e1e9239a03
Devalekhaa <50616161+Devalekhaa@users.noreply.github.com>
2019-06-05 19:28:53 +0530

    Update play29.py

--- play29.py
+++ play29.py
@@ -1,11 +1,10 @@
-d,r=map(int,input().split())
-e=0
-for i in range(d,v+1):
-  a=2
-  while a<=v:
-    if i==a*a:
-       e=e+1 
-       break
-    a+=1
-print(e)
-
+try:
+	d,e=map(int,input().split(" "))
+	v=0
+	for i in range(d,e+1):
+		for j in range(1,i+1):
+			if j**2==i:
+				v+=1
+	print(v)
+except ValueError:
+	print("invalid")
