fe34e6a2a4c7fb35df819def814fe72d1e4c2ff5
Devalekhaa <50616161+Devalekhaa@users.noreply.github.com>
2019-06-13 11:54:15 +0530

    Update set102.py

--- set102.py
+++ set102.py
@@ -1,7 +1,7 @@
-len,w,h=input().split()
-len=int(len)
-w=int(w)
-h=int(h)
-tsa=2*((len*w)+(w*h)+(h*len))
-volume=len*w*h
-print(tsa,volume)
+d,e,f=input().split()
+d=int(d)
+e=int(e)
+f=int(f)
+t=2*((d*e)+(e*f)+(f*d))
+vol=d*e*f
+print(t,vol)
